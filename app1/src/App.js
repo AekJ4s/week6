faef65ad1e1fb757509600c9e345662aafcf0e38
AekJ4s <Jasdakornam@gmail.com>
2024-01-19 15:07:08 +0700

    StateClass&Func

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import Button from './class-components'
-import { Calculator } from './calculator'
-import Calculator2 from './calculator';
-import EventData1 from './event-data'
-import { EventData2 } from './event-data'
-import { Table } from './event-data'
-import RefsClass from './refs-class';
-import RefsFunc from './refs-func';
-import Refsarray from './refs-array';
-
-export default function App(){
-    return <Refsarray/>
-      
+// import Button from './class-components'
+// import { Calculator } from './calculator'
+// import Calculator2 from './calculator';
+// import EventData1 from './event-data'
+// import { EventData2 } from './event-data'
+// import { Table } from './event-data'
+// import RefsClass from './refs-class';
+// import RefsFunc from './refs-func';
+// import Refsarray from './refs-array';
+import Cart from './state-class'
+import StateFunc from './state-func'
+export default function App() {
+return <StateFunc/>
 }
+
 
---
+++ app1/src/state-func.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+export default function StateFunc(){
+    let [text, setText] = useState('Hello World')
+    let [size, setSize] = React.useState(16)
+    const onClickSetText = () => {
+        let t = prompt('กำหนดข้อความ')
+        if (t) {
+            setText(t)
+        }
+    }
+    const onClickZoomIn = () => {
+        let newSize = size + 1
+        setSize(+1)
+    }
+    let msgboxSyle = {
+        display: 'inline-block',
+        width: 350,
+        fontSize: size, //ใช้ค่าจาก State
+        backgroundColor: '#ccc',
+        padding: 5,
+        textAlign: 'left'
+    }
+    return (
+        <div style={{ textAlign: 'center', marginTop: 20 }}>
+            <div style={msgboxSyle}>{text}</div><br /><br />
+            <button onClick={onClickSetText}> ข้อความ</button> &nbsp;
+            <button onClick={() => setSize(size + 1)}>เพิ่มขนาดตัวหนังสือ</button> &nbsp;
+            <button onClick={() => setSize(size - 1)}>ลดขนาดตัวหนังสือ</button>
+        </div >
+    )
+}
