a26cb95d92d01349b1c799c92e680da1dad240e5
AekJ4s <Jasdakornam@gmail.com>
2024-01-19 14:26:21 +0700

    Create refs-func

--- app1/src/App.js
+++ app1/src/App.js
@@ -5,32 +5,11 @@ import Calculator2 from './calculator';
 import EventData1 from './event-data'
 import { EventData2 } from './event-data'
 import { Table } from './event-data'
-
+import RefsClass from './refs-class';
+import RefsFunc from './refs-func';
 
 export default function App(){
-    return (
-        <>
-        
-        <br></br>
-        <center>Button</center>
-          <EventData2/>
-          <br></br>
-          <hr></hr>
-
-        <center >Calculator</center>
-          <Calculator2 />
-          <br></br>
-          <hr></hr>
-
-          <br></br>
-        <center>Table</center>
-          <Table />
-          <br />
-          <hr></hr>
-
-          <br/>
-
-        </>
-      );
+    return <RefsFunc/>
+      
 }
 
